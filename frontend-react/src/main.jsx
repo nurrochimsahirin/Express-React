40e3dbde2a59a6cb1d77ccd80fb39478daeb7b60
nurrochimsahirin <nurrochimsahirin@gmail.com>
2024-06-05 13:34:32 +0700

    perubahan pertamaku

---
+++ frontend-react/src/main.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+//import BrowserRouter dari react router
+import { BrowserRouter } from "react-router-dom";
+
+//import AuthProvider
+import { AuthProvider } from "./context/AuthContext";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>
+);
